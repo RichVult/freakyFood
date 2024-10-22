579bd58cf53fd921bb47cf9307992253f27a04e1
TylerHerlihy1 <143517685+TylerHerlihy1@users.noreply.github.com>
2024-10-22 17:43:46 -0400

    yolo

---
+++ project/db/schema/Menu.py
@@ -0,0 +1,27 @@
+"""Menu.py: create a table named Menu in the freakyfood database"""
+from db.server import db
+
+class Menu(db.Model):
+    __tablename__='Menu'
+    MenuID=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    MenuID=db.Column(db.Integer, db.ForeignKey('Store.StoreID'), nullable=False)
+    StoreID=db.Column(db.String)
+
+    #define relationship
+    menu_id=db.relationship('StoreID', back_populates='Menu')
+
+    def __init__(self, newStoreID, newMenuID):
+        self.MenuID=newMenuID
+        self.newStoreID=newStoreID
+        
+        pass
+
+    def __repr__ (self):
+        #add text to f-string
+        return f"""
+            StoreID : {self.StoreID}
+            MenuID : {self.MenuID}
+        """
+    
+    def __repr__(self):
+        return self.__repr__()
