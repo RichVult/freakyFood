911b94e55c38d3203ad1646a2a4a8be407c2af7b
djgd27 <david.galindodelgado1@marist.edu>
2024-10-22 17:50:50 -0400

    fixed issue with menu-item schema, i am awaiting for menu table before my code will work, relation relies on the menu entity

---
+++ project/db/schema/MenuItems.py
@@ -0,0 +1,37 @@
+"""MenuItems.py: create a table named MenuItems in the freakyfood database"""
+from db.server import db
+
+
+class MenuItems(db.Model):
+    __tablename__ = 'MenuItems'
+    MenuItemID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    MenuID = db.Column(db.Integer, db.ForeignKey(
+        'Menu.MenuID'), nullable=False)
+    MIName = db.Column(db.String(120), nullable=False)
+    MIDesc = db.Column(db.String(255), nullable=False)
+    MICal = db.Column(db.Integer, nullable=False)
+    MIPrice = db.Column(db.Float, nullable=False)
+
+    # Define relationship
+    menu = db.relationship('Menu', backref=db.backref('menu_items', lazy=True))
+
+    def __init__(self, newMenuItemID, newMenuID, newMIName, newMIDesc, newMICal, newMIPrice):
+        self.MenuItemID = newMenuItemID
+        self.MenuID = newMenuID
+        self.MIName = newMIName
+        self.MIDesc = newMIDesc
+        self.MICal = newMICal
+        self.MIPrice = newMIPrice
+
+    def __repr__(self):
+        return f"""
+        MenuItemID : {self.MenuItemID}
+        MenuID : {self.MenuID}
+        MIName : {self.MIName}
+        MIDesc : {self.MIDesc}
+        MICal : {self.MICal}
+        MIPrice : {self.MIPrice}
+    """
+
+    def __repr__(self):
+        return self.__repr__()
