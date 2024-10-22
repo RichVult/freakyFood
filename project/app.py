a48c1eed3b694d191d528d22e5668522065b077e
djgd27 <david.galindodelgado1@marist.edu>
2024-10-22 19:50:21 -0400

    bare bones login page with minimal validation, needs to be tested. next steps is adding dummy user data to test login page

--- project/app.py
+++ project/app.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import request, render_template
 from db.server import *  # import all from server
+from sqlalchemy import text
+
+from db.server import app, db
+
+from db.schema.Users import Users
+
 
 app = Flask(__name__, template_folder='templates')  # Set the template folder
 
+
 @app.route('/')
 def index():
-    return render_template('FFHomePage.html')
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])  # +
+def login():
+    if request.method == 'POST':
+
+        try:
+
+            username = request.form.get("email")
+            password = request.form.get("password")
+
+            db_password = db.session.execute(text("""SELECT "Password" FROM "Users" WHERE "Email" = :username"""), {
+                "username": username}).fetchone()
+
+            if db_password is None:
+                return render_template('fail.html')
+
+            if db_password[0] == password:
+                print("Login Successful")
+                return render_template('success.html')
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
